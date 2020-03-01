1b07811993225569830fba9239d899430dc554a6
Lars Noschinski <noschinl@gmail.com>
2020-03-01 20:20:53 +0100

    Optimize tag_matched.
    The previous implementation iterates over the list of valid tags for
    each call. Using a dictionary with priorities instead improves
    performance (by more than a factor 4 on my machine).

--- poiconverter/tagfilter.py
+++ poiconverter/tagfilter.py
@@ -4,17 +4,27 @@ checks which entries in input file should be forwarded to output database
 """
 class TagFilter:
     def __init__(self, filter_file):
-        self.valid_tags = []
+        # Dictionary of the valid tags. The key is the tag, the value the inverse priority (lower values mean higher priority)
+        self.valid_tags = {}
         with open(filter_file,'r') as f:
             line = f.readline()
+            prio = 0
             while line:
                 if not line.startswith('#') and len(line) > 2:
-                    self.valid_tags.append(line.strip().split('='))
+                    tag = line.strip().split('=')
+                    self.valid_tags[(tag[0],tag[1])] = prio
+                    prio += 1
                 line = f.readline()            
 
+    """
+    returns the matching tag with the highest priority (or None)
+    """
     def tag_matched(self, tags):
-        for tag in self.valid_tags:
-            if tag[0] in tags.keys():
-                if tag[1] == tags.get(tag[0], ''):
-                    return (tag[0], tag[1])
-        return None
+        matched_tag = None
+        matched_prio = None
+        for tag in tags:
+            prio = self.valid_tags.get(tag)
+            if prio is not None and (matched_prio is None or prio < matched_prio):
+                matched_tag = tag
+                matched_priority = prio
+        return matched_tag
